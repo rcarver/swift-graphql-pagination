49d3239c2ef01da88ab192a3ce9c6784367a10bb
Ryan Carver <ryan@ryancarver.com>
2024-04-23 16:53:27 -0700

    Initial Commit

---
+++ Tests/swift-plus2paginationTests/swift_plus2paginationTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_plus2pagination
+
+final class swift_plus2paginationTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
