b93017e930ffa4cc3c75a98769fa7dc38125b4bb
Ryan Carver <ryan@ryancarver.com>
2024-04-23 18:19:34 -0700

    forward pagination

--- Sources/GraphQLPagination/EdgeBuilder.swift
+++ Sources/GraphQLPagination/EdgeBuilder.swift
@@ -31,7 +31,7 @@ extension EdgeBuilder {
         }
         switch cursor {
         case .identifier:
-            let edges = nodes.enumerated().map { self.transform($1, $1.cursor, $0) }
+            let edges = self.nodes.enumerated().map { self.transform($1, $1.cursor, $0) }
             return EdgesConstruction(edges: edges, pageInfo: GraphPageInfo(
                 hasPreviousPage: false,
                 hasNextPage: false,
@@ -39,7 +39,7 @@ extension EdgeBuilder {
                 endCursor: nodes.last?.cursor
             ))
         case .index:
-            let edges = nodes.enumerated().map { self.transform($1, Cursor(intValue: $0), $0) }
+            let edges = self.nodes.enumerated().map { self.transform($1, Cursor(intValue: $0), $0) }
             return EdgesConstruction(edges: edges, pageInfo: GraphPageInfo(
                 hasPreviousPage: false,
                 hasNextPage: false,
@@ -48,87 +48,52 @@ extension EdgeBuilder {
             ))
         }
     }
-//    func makeEdges<P: GraphPaginatable>(pagionation: P) -> EdgesConstruction<Edge> {
-//
-//    }
-}
-
-/// Turns a set of nodes and pagination in to edges.
-func makeEdges<Node: GraphCursorable, P: GraphPaginatable, Edge>(
-    nodes: [Node],
-    pagination: P?,
-    cursor: CursorType,
-    makeEdge: (Cursor, Node, Int) -> Edge
-) -> (edges: [Edge], pageInfo: GraphPageInfo) {
-    if let pagination {
+    func makeEdges<P: GraphForwardPaginatable>(cursor: CursorType, pagination: P) -> EdgesConstruction<Edge> {
         switch cursor {
         case .identifier:
             let calculator = PlusTwoPageInfoCalculator(
                 cursors: nodes.map(\.cursor),
                 pagination: pagination
             )
-            let edges = calculator.mapCursors(nodes: nodes, transform: makeEdge)
-            let pageInfo = calculator.makePageInfo()
-            return (edges, pageInfo)
+            return EdgesConstruction(
+                edges: calculator.mapCursors(nodes: self.nodes, transform: self.transform),
+                pageInfo: calculator.makePageInfo()
+            )
         case .index:
             let calculator = PlusTwoPageInfoCalculator(
-                cursors: nodes.indices.map {
-                    switch pagination.current {
-                    case let .forward(forward):
-                        let after = forward.after?.intValue()
-                        return Cursor(intValue: after == nil ? $0 : after! + $0)
-                    case let .backward(backward):
-                        return Cursor(intValue: $0)
-                    case .none:
-                        return Cursor(intValue: $0)
-                    }
+                cursors: self.nodes.indices.map {
+                    let after = pagination.after?.intValue()
+                    return Cursor(intValue: after == nil ? $0 : $0)
                 },
                 pagination: pagination
             )
-            let edges = calculator.mapIndexedCursors(nodes: nodes, transform: makeEdge)
-            let pageInfo = calculator.makePageInfo()
-            return (edges, pageInfo)
-        }
-    } else {
-        switch cursor {
-        case .identifier:
-            let edges = nodes.enumerated().map { makeEdge($1.cursor, $1, $0) }
-            return (edges, .zero)
-        case .index:
-            let edges = nodes.enumerated().map { makeEdge(Cursor(intValue: $0), $1, $0) }
-            return (edges, .zero)
+            return EdgesConstruction(
+                edges: calculator.mapIndexedCursors(nodes: self.nodes, transform: self.transform),
+                pageInfo: calculator.makePageInfo()
+            )
         }
     }
 }
 
-private struct PlusTwoPageInfoCalculator<Pagination: GraphPaginatable> {
+private struct PlusTwoPageInfoCalculator<Pagination: GraphForwardPaginatable> {
     var cursors: [Cursor]
     var pagination: Pagination
 }
 
 extension PlusTwoPageInfoCalculator {
-    func mapCursors<T, N>(nodes: [T], transform: (Cursor, T, Int) -> N) -> [N] {
-        zip(self.sliced(self.cursors), self.sliced(nodes)).enumerated().map { transform($1.0, $1.1, $0) }
+    func mapCursors<T, N>(nodes: [T], transform: (T, Cursor, Int) -> N) -> [N] {
+        zip(self.sliced(self.cursors), self.sliced(nodes)).enumerated().map {
+            transform($1.1, $1.0, $0)
+        }
     }
-    func mapIndexedCursors<T, N>(nodes: [T], transform: (Cursor, T, Int) -> N) -> [N] {
-        switch self.pagination.current {
-        case let .forward(forward):
-            if let after = forward.after?.intValue() {
-                return self.sliced(nodes).enumerated().enumerated().map {
-                    transform(Cursor(intValue: after + 1 + $1.0), $1.1, $0)
-                }
-            } else {
-                return self.sliced(nodes).enumerated().map {
-                    transform(Cursor(intValue: $0), $1, $0)
-                }
-            }
-        case .backward:
+    func mapIndexedCursors<T, N>(nodes: [T], transform: (T, Cursor, Int) -> N) -> [N] {
+        if let after = self.pagination.after?.intValue() {
             return self.sliced(nodes).enumerated().map {
-                transform(Cursor(intValue: $0), $1, $0)
+                transform($0.element, Cursor(intValue: after + 1 + $0.offset), $0.offset)
             }
-        case .none:
+        } else {
             return self.sliced(nodes).enumerated().map {
-                transform(Cursor(intValue: $0), $1, $0)
+                transform($1, Cursor(intValue: $0), $0)
             }
         }
     }
@@ -145,52 +110,19 @@ extension PlusTwoPageInfoCalculator {
 
 private extension PlusTwoPageInfoCalculator {
     var hasPreviousPage: Bool {
-        switch self.sliceInfo {
-        case let .forward(sliceInfo):
-            if let index = sliceInfo.afterIndex {
-                return index > 0
-            } else {
-                return false
-            }
-        case let .backward(sliceInfo):
-            switch (sliceInfo.beforeIndex, sliceInfo.count) {
-            case let (.some(index), .some(count)):
-                return index - count > 0
-            case (.some, .none):
-                return false
-            case let (.none, .some(count)):
-                return count < self.cursors.count
-            case (.none, .none):
-                return false
-            }
-        }
+        sliceInfo.afterIndex != nil
     }
     var hasNextPage: Bool {
-        switch self.sliceInfo {
-        case let .forward(sliceInfo):
-            guard let first = pagination.first else {
-                return false
-            }
-            if sliceInfo.afterIndex != nil {
-                return cursors.count >= first + 2
-            } else {
-                return cursors.count >= first + 1
-            }
-        case let .backward(sliceInfo):
-            return sliceInfo.beforeIndex != nil
+        guard let first = pagination.first else {
+            return false
+        }
+        if sliceInfo.afterIndex != nil {
+            return cursors.count >= first + 2
+        } else {
+            return cursors.count >= first + 1
         }
     }
-    enum SliceInfo {
-        case forward(ForwardSliceInfo)
-        case backward(BackwardSliceInfo)
-        func slice<T>(_ collection: [T]) -> ArraySlice<T> {
-            switch self {
-            case let .forward(s): s.slice(collection)
-            case let .backward(s): s.slice(collection)
-            }
-        }
-    }
-    struct ForwardSliceInfo {
+    struct SliceInfo {
         var afterIndex: Int?
         var prefix: Int?
         func slice<T>(_ collection: [T]) -> ArraySlice<T> {
@@ -206,50 +138,17 @@ private extension PlusTwoPageInfoCalculator {
             }
         }
     }
-    struct BackwardSliceInfo {
-        var beforeIndex: Int?
-        var count: Int?
-        func slice<T>(_ collection: [T]) -> ArraySlice<T> {
-            switch (self.beforeIndex, self.count) {
-            case let (.some(beforeIndex), .some(count)):
-                return collection[0..<beforeIndex].suffix(count)
-            case let (.some(beforeIndex), .none):
-                guard beforeIndex > 0 else { return [] }
-                return collection[0..<beforeIndex]
-            case let (.none, .some(prefix)):
-                return collection.suffix(prefix)
-            case (.none, .none):
-                return ArraySlice(collection)
-            }
-        }
-    }
     var sliceInfo: SliceInfo {
-        switch self.pagination.current {
-        case let .forward(forward):
-            guard
-                let after = forward.after,
-                let afterIndex = cursors.firstIndex(where: { $0 == after })
-            else {
-                return .forward(ForwardSliceInfo(prefix: forward.first))
-            }
-            return .forward(ForwardSliceInfo(
-                afterIndex: afterIndex.advanced(by: 1),
-                prefix: forward.first
-            ))
-        case let .backward(backward):
-            guard
-                let before = backward.before,
-                let beforeIndex = cursors.lastIndex(where: { $0 == before })
-            else {
-                return .backward(BackwardSliceInfo(count: backward.last))
-            }
-            return .backward(BackwardSliceInfo(
-                beforeIndex: beforeIndex,
-                count: backward.last
-            ))
-        case .none:
-            return .forward(ForwardSliceInfo())
+        guard
+            let after = pagination.after,
+            let afterIndex = cursors.firstIndex(where: { $0 == after })
+        else {
+            return SliceInfo(prefix: pagination.first)
         }
+        return SliceInfo(
+            afterIndex: afterIndex.advanced(by: 1),
+            prefix: pagination.first
+        )
     }
     func sliced<T>(_ nodes: [T]) -> ArraySlice<T> {
         precondition(nodes.count == cursors.count, "Node count must equal cursor count")
--- Tests/GraphQLPaginationTests/EdgeBuilderTests.swift
+++ Tests/GraphQLPaginationTests/EdgeBuilderTests.swift
@@ -19,9 +19,7 @@ final class EdgeBuilderTests: XCTestCase {
     let c = TestNode(id: "c")
 
     func test_makeEdges() {
-        let builder = EdgeBuilder(nodes: [a, b, c]) { node, cursor, index in
-            TestEdge(node: node, cursor: cursor, index: index)
-        }
+        let builder = EdgeBuilder(nodes: [a, b, c], transform: TestEdge.init)
         XCTAssertNoDifference(
             builder.makeEdges(cursor: .identifier),
             EdgesConstruction(
@@ -56,9 +54,7 @@ final class EdgeBuilderTests: XCTestCase {
         )
     }
     func test_makeEdges_empty() {
-        let builder = EdgeBuilder(nodes: []) { node, cursor, index in
-            TestEdge(node: node, cursor: cursor, index: index)
-        }
+        let builder = EdgeBuilder(nodes: [], transform: TestEdge.init)
         XCTAssertNoDifference(
             builder.makeEdges(cursor: .identifier),
             EdgesConstruction(
@@ -80,6 +76,197 @@ final class EdgeBuilderTests: XCTestCase {
                     hasNextPage: false,
                     startCursor: nil,
                     endCursor: nil
+                )
+            )
+        )
+    }
+}
+
+final class EdgeBuilderForwardPaginationTests: XCTestCase {
+
+    struct TestNode: Equatable, GraphCursorable {
+        let id: String
+        var cursor: Cursor { .init(rawValue: self.id) }
+    }
+    struct TestEdge: Equatable {
+        let node: TestNode
+        let cursor: Cursor
+        let index: Int
+    }
+    struct Forward: GraphForwardPaginatable {
+        var first: Int?
+        var after: Cursor?
+    }
+    let a = TestNode(id: "a")
+    let b = TestNode(id: "b")
+    let c = TestNode(id: "c")
+    let d = TestNode(id: "d")
+
+    func test_makeEdges_first() {
+        let builder = EdgeBuilder(nodes: [a, b, c, d], transform: TestEdge.init)
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .identifier, pagination: Forward(first: 3)),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: a, cursor: "a", index: 0),
+                    TestEdge(node: b, cursor: "b", index: 1),
+                    TestEdge(node: c, cursor: "c", index: 2),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: false,
+                    hasNextPage: true,
+                    startCursor: "a",
+                    endCursor: "c"
+                )
+            )
+        )
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .index, pagination: Forward(first: 3)),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: a, cursor: 0, index: 0),
+                    TestEdge(node: b, cursor: 1, index: 1),
+                    TestEdge(node: c, cursor: 2, index: 2),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: false,
+                    hasNextPage: true,
+                    startCursor: 0,
+                    endCursor: 2
+                )
+            )
+        )
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .identifier, pagination: Forward(first: 4)),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: a, cursor: "a", index: 0),
+                    TestEdge(node: b, cursor: "b", index: 1),
+                    TestEdge(node: c, cursor: "c", index: 2),
+                    TestEdge(node: d, cursor: "d", index: 3),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: false,
+                    hasNextPage: false,
+                    startCursor: "a",
+                    endCursor: "d"
+                )
+            )
+        )
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .index, pagination: Forward(first: 4)),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: a, cursor: 0, index: 0),
+                    TestEdge(node: b, cursor: 1, index: 1),
+                    TestEdge(node: c, cursor: 2, index: 2),
+                    TestEdge(node: d, cursor: 3, index: 3),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: false,
+                    hasNextPage: false,
+                    startCursor: 0,
+                    endCursor: 3
+                )
+            )
+        )
+    }
+    func test_makeEdges_after() {
+        let builder = EdgeBuilder(nodes: [a, b, c, d], transform: TestEdge.init)
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .identifier, pagination: Forward(after: "a")),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: b, cursor: "b", index: 0),
+                    TestEdge(node: c, cursor: "c", index: 1),
+                    TestEdge(node: d, cursor: "d", index: 2),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: true,
+                    hasNextPage: false,
+                    startCursor: "b",
+                    endCursor: "d"
+                )
+            )
+        )
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .index, pagination: Forward(after: 0)),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: b, cursor: 1, index: 0),
+                    TestEdge(node: c, cursor: 2, index: 1),
+                    TestEdge(node: d, cursor: 3, index: 2),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: true,
+                    hasNextPage: false,
+                    startCursor: 1,
+                    endCursor: 3
+                )
+            )
+        )
+    }
+    func test_makeEdges_first_after() {
+        let builder = EdgeBuilder(nodes: [a, b, c, d], transform: TestEdge.init)
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .identifier, pagination: Forward(first: 2, after: "a")),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: b, cursor: "b", index: 0),
+                    TestEdge(node: c, cursor: "c", index: 1),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: true,
+                    hasNextPage: true,
+                    startCursor: "b",
+                    endCursor: "c"
+                )
+            )
+        )
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .index, pagination: Forward(first: 2, after: 0)),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: b, cursor: 1, index: 0),
+                    TestEdge(node: c, cursor: 2, index: 1),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: true,
+                    hasNextPage: true,
+                    startCursor: 1,
+                    endCursor: 2
+                )
+            )
+        )
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .identifier, pagination: Forward(first: 3, after: "a")),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: b, cursor: "b", index: 0),
+                    TestEdge(node: c, cursor: "c", index: 1),
+                    TestEdge(node: d, cursor: "d", index: 2),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: true,
+                    hasNextPage: false,
+                    startCursor: "b",
+                    endCursor: "d"
+                )
+            )
+        )
+        XCTAssertNoDifference(
+            builder.makeEdges(cursor: .index, pagination: Forward(first: 3, after: 0)),
+            EdgesConstruction(
+                edges: [
+                    TestEdge(node: b, cursor: 1, index: 0),
+                    TestEdge(node: c, cursor: 2, index: 1),
+                    TestEdge(node: d, cursor: 3, index: 2),
+                ],
+                pageInfo: GraphPageInfo(
+                    hasPreviousPage: true,
+                    hasNextPage: false,
+                    startCursor: 1,
+                    endCursor: 3
                 )
             )
         )
